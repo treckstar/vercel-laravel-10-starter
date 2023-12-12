a95a8f317d88692ff3fbe55c8c9373fe8ad49716
Brandon Trecki <treckstar@gmail.com>
2023-12-12 13:31:42 -0500

    Init

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+      "routes/web.php",
+      "resources/**/*.blade.php"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
